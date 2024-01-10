9c5d0f4e31dc8a6049f9d2c25630f43d5e1d70f6
Sefa Basnak <107328046+sefabasnak@users.noreply.github.com>
2024-01-10 20:23:36 +0300

    Update sitemapper.py

--- sitemapper.py
+++ sitemapper.py
@@ -3,15 +3,13 @@ from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urljoin
 
 def get_all_pages(domain):
-    # Belirli bir domaindeki tüm sayfaları bulan fonksiyon
     all_pages = set()
     base_url = f"http://{domain}"
 
     def crawl_page(url):
-        # Sayfadaki linkleri çeken fonksiyon
         try:
             response = requests.get(url, timeout=5)
-            response.raise_for_status()  # Hata durumunda hemen programı sonlandır
+            response.raise_for_status()
         except requests.exceptions.RequestException as e:
             print(f"Hata: {e} - {url}")
             return
@@ -20,12 +18,10 @@ def get_all_pages(domain):
         for a_tag in soup.find_all('a', href=True):
             link = a_tag['href']
             absolute_url = urljoin(url, link)
-            # Sadece belirli bir domain içindeki linkleri ekleyin
             if urlparse(absolute_url).hostname == domain:
                 all_pages.add(absolute_url)
 
     def crawl_all_pages(url):
-        # Tüm sayfalarda dolaşma
         try:
             crawl_page(url)
             for page in list(all_pages):
@@ -37,11 +33,9 @@ def get_all_pages(domain):
 
     return all_pages
 
-# Kullanıcıdan domaini al
 domain = input("Lütfen domaini girin (örneğin: example.com): ")
 all_pages = get_all_pages(domain)
 
-# Bulunan sayfaları yazdırma
 for page in all_pages:
     print(page)
 
